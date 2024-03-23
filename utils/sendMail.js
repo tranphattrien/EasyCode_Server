b48457bb0bce16b870957abff8bdfe0435c9f688
Tran Phat Trien <trientranphat@gmail.com>
2024-03-23 19:50:36 +0700

    update

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,12 +1,11 @@
 const nodemailer = require("nodemailer");
-const sendGridTransport = require("nodemailer-sendgrid-transport");
 
 const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
     service: process.env.SMPT_HOST_SERVICE,
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
-    secure: 465,
+    secure: true,
     auth: {
       user: process.env.SMPT_MAIL,
       pass: process.env.SMPT_PASSWORD
