591e53833df2a2dba959bfcc2baa16f82bd705a3
Tran Phat Trien <trientranphat@gmail.com>
2024-07-22 19:20:17 +0700

    Change Password function

--- controllers/auth.js
+++ controllers/auth.js
@@ -292,3 +292,66 @@ exports.postGoogleAuth = async (req, res) => {
       });
     });
 };
+
+exports.changePassword = (req, res) => {
+  let { currentPassword, newPassword } = req.body;
+
+  if (
+    !passwordRegex.test(currentPassword) ||
+    !passwordRegex.test(newPassword)
+  ) {
+    return res.status(403).json({
+      error:
+        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters!"
+    });
+  }
+
+  User.findOne({ _id: req.user })
+    .then((user) => {
+      if (user.google_auth) {
+        return res.status(403).json({
+          error:
+            "You can't change account's password because you logged in through google"
+        });
+      }
+      bcrypt.compare(
+        currentPassword,
+        user.personal_info.password,
+        (err, result) => {
+          if (err) {
+            return res.status(500).json({
+              error:
+                "Some error occured while changing the password, please try again later !"
+            });
+          }
+
+          if (!result) {
+            return res
+              .status(403)
+              .json({ error: "Incorrect current password !" });
+          }
+          bcrypt.hash(newPassword, 12, (err, hashed_password) => {
+            User.findOneAndUpdate(
+              { _id: req.user },
+              { "personal_info.password": hashed_password }
+            )
+              .then((u) => {
+                return res
+                  .status(200)
+                  .json({ status: "Password changed successful!" });
+              })
+              .catch((err) => {
+                return res.status(500).json({
+                  error:
+                    "Some error occured while saving new password, Please try again later !"
+                });
+              });
+          });
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: "User not found !" });
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const authController = require("../controllers/auth");
-
+const { verifyJWT } = require("../middlewares/auth");
 const router = express.Router();
 
 router.post("/signup", authController.postSignup);
@@ -10,5 +10,7 @@ router.post("/activation", authController.activateAccount);
 router.post("/signin", authController.postSignin);
 
 router.post("/google-auth", authController.postGoogleAuth);
+
+router.post("/change-password", verifyJWT, authController.changePassword);
 
 module.exports = router;
