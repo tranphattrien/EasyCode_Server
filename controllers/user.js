5e4066a33ebdb3d31f2f07545dea6eec07689170
Tran Phat Trien <trientranphat@gmail.com>
2024-07-23 03:43:05 +0700

    edit profile

--- controllers/user.js
+++ controllers/user.js
@@ -27,3 +27,71 @@ exports.postGetProfile = (req, res) => {
       return res.status(500).json({ error: error.message });
     });
 };
+
+exports.updateProfileImg = (req, res) => {
+  let { url } = req.body;
+  User.findOneAndUpdate({ _id: req.user }, { "personal_info.profile_img": url })
+    .then(() => {
+      return res.status(200).json({ profile_img: url });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+};
+
+exports.updateProfile = (req, res) => {
+  let { username, bio, social_links } = req.body;
+  let bioLimit = 150;
+
+  if (username.length < 3) {
+    return res
+      .status(403)
+      .json({ error: "Username should be at least 3 letters long!" });
+  }
+
+  if (bio.length > bioLimit) {
+    return res
+      .status(403)
+      .json({ error: `Bio should not be more than ${bioLimit} characters!` });
+  }
+
+  let socialLinksArr = Object.keys(social_links);
+  try {
+    for (let i = 0; i < socialLinksArr.length; i++) {
+      if (social_links[socialLinksArr[i]].length) {
+        let hostname = new URL(social_links[socialLinksArr[i]]).hostname;
+        if (
+          !hostname.includes(`${socialLinksArr[i]}.com`) &&
+          socialLinksArr[i] !== "website"
+        ) {
+          return res.status(403).json({
+            error: `${socialLinksArr[i]} link is invalid. You must enter a full links !`
+          });
+        }
+      }
+    }
+  } catch (err) {
+    return res.status(500).json({
+      error: "You must provide full social links with http(s) included !"
+    });
+  }
+
+  let updateObj = {
+    "personal_info.username": username,
+    "personal_info.bio": bio,
+    social_links
+  };
+
+  User.findOneAndUpdate({ _id: req.user }, updateObj, {
+    runValidators: true
+  })
+    .then(() => {
+      return res.status(200).json({ username });
+    })
+    .catch((err) => {
+      if (err.code == 11000) {
+        return res.status(409).json({ error: "Username is already taken!" });
+      }
+      return res.status(500).json({ error: err.message });
+    });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const userController = require("../controllers/user");
+const { verifyJWT } = require("../middlewares/auth");
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/search-users", userController.postSearchUser);
 
 router.post("/get-profile", userController.postGetProfile);
 
+router.post("/update-profile-img", verifyJWT, userController.updateProfileImg);
+router.post("/update-profile", verifyJWT, userController.updateProfile);
 
 module.exports = router;
